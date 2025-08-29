e645ada600006a1e179431c0c9407af0f4ee2333
Harshal Dulera <111957198+harshaldulera@users.noreply.github.com>
2025-08-29 12:07:45 +0530

    Update MyCalc.java

--- MyCalc.java
+++ MyCalc.java
@@ -1,1 +1,18 @@
-
+public class MyCalc {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java MyCalc <num1> <num2>");
+            System.exit(1);
+        }
+        try {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int sum = a + b;
+            System.out.println("Inputs: " + a + " and " + b);
+            System.out.println("Sum: " + sum);
+        } catch (NumberFormatException e) {
+            System.out.println("Both arguments must be integers.");
+            System.exit(2);
+        }
+    }
+}
